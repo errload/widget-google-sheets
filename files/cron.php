34ece7f7f5b4991e8fdae79b74547cfcff14a3f0
errload <errload@mail.ru>
2023-03-29 18:25:54 +0300

    date diff 15 -> 20 min

--- files/cron.php
+++ files/cron.php
@@ -11,13 +11,13 @@ if (file_exists('google_sheets/error_log')) {
         unlink('google_sheets/error_log');
 }
 
-// проверка файлов старта и паузы на наличие багов (жизни файла более 15 минут)
+// проверка файлов старта и паузы на наличие багов (жизни файла более 20 минут)
 if (file_exists('google_sheets/start')) {
     $file_start = filemtime('google_sheets/start');
     $file_start = date('Y-m-d H:i:s', $file_start);
     $diff = (new DateTime())->diff(new DateTime($file_start));
 
-    if ($diff->i > 15) unlink('google_sheets/start');
+    if ($diff->i > 20) unlink('google_sheets/start');
 }
 
 if (file_exists('google_sheets/pause')) {
@@ -25,7 +25,7 @@ if (file_exists('google_sheets/pause')) {
     $file_pause = date('Y-m-d H:i:s', $file_pause);
     $diff = (new DateTime())->diff(new DateTime($file_pause));
 
-    if ($diff->i > 15) unlink('google_sheets/pause');
+    if ($diff->i > 20) unlink('google_sheets/pause');
 }
 
 if (!file_exists('google_sheets/start')) {
